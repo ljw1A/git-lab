2074acb4baa1e98a29583cba01b8fe8264e9d17d
ljw1A <3355428852@qq.com>
2025-11-21 11:37:10 +0800

    feat: add sum function

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
-//Todo: Add more greetings
+//Todo: Add sum function
+int sum(int a, int b) {
+    return a + b;
+}
 int main() {
     cout << "Hello, Git!" << endl;
     cout<<"Hello, GitHub!"<<endl;
